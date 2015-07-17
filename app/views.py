a10d20aae86cee9917c0ab76d16b5a47a975bae9
ganeshredcobra <ganeshredcobra@gmail.com>
2015-07-17 23:32:56 +0530

    Adding simple HTML

--- app/views.py
+++ app/views.py
@@ -3,4 +3,22 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	return '<h1>Hello!World<h1>'
+    user = {'nickname': 'Miguel'}  # fake user
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
+	
+@app.route("/members")
+def members():
+    return "Members"
+ 
+@app.route("/members/<string:name>/")
+def getMember(name):
+    return '<h1>Hello {{%s}}</h1>'%name
