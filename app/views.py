d0bfcbd642d08fc3b6e9f0807a960b9b0c361555
ganeshredcobra <ganeshredcobra@gmail.com>
2015-07-18 00:07:16 +0530

    Loops in templates

--- app/views.py
+++ app/views.py
@@ -5,7 +5,20 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html',user=user)
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
 	
 @app.route("/members")
 def members():
